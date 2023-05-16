2ddf33b9ea8af4451104575c1cd4a6de2e585e53
Rimbelghali <rime.belghali@gmail.com>
2023-05-16 14:19:15 +0000

    pratice again

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ long int b;
 long long int c;
 char d;
 float f;
+printf("Size of a char:%zu byte(s)\n", sizeof(d));
 printf("Size of an int:%zu byte(s)\n", sizeof(a));
 printf("Size of a long int:%zu byte(s)\n", sizeof(b));
 printf("Size of a long long int:%zu byte(s)\n", sizeof(c));
-printf("Size of a char:%zu byte(s)\n", sizeof(d));
 printf("Size of a float:%zu byte(s)\n", sizeof(f));
 return 0;
 } 
