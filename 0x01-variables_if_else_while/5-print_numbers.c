78075c301730d0710cbc5b9a985aa1db27300df7
Rimbelghali <rime.belghali@gmail.com>
2023-05-19 11:05:55 +0000

    pratice again

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 /**
- * main - Prints the numbers from 0-9.
-(*
- * Return: 0 on success
- */
+* main – write numbers from 0 to 9
+*
+* Return: 0 on success
+**/
 int main(void)
 {
-	printf("0123456789\n");
-	return (0);
+char x = '1';
+while (x <= '9')
+{
+putchar(x);
+x++;
+}
+putchar('\n');
+return (0);
 }
