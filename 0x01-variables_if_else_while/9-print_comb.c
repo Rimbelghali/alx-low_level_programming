35d35ff0bc4c4ec5b9b10a170d574f78cbed1c22
Rimbelghali <rime.belghali@gmail.com>
2023-05-21 22:55:39 +0000

    pratice again

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
-
 /**
-* main - entry point
-* Return: always 0 (success)
-*/
+ *main - Entry point
+ *Return:0 on success
+ */
 int main(void)
 {
-int n;
-
-n = 48;
-
-while (n <= 57) /*57 corresponds with ASCII character 9*/
-{
-putchar (n);
-if (n != 57)
-{
-putchar(',');
-putchar(' ');
-}
-n++;
-}
-putchar ('\n'); /*add new line*/
-return (0);
+  int i;
+    i = '0';
+  while (i <= '9')
+    {
+    putchar (i);
+  if (i != '9')
+    {  
+  putchar(',');
+    putchar(' ');
+    }
+  i++;
+    }
+    putchar('\n');
+    return (0);
 }
