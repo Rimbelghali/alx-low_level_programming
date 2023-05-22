cd5b91b33e93ae7b529430953543c118e1d2b33e
Rimbelghali <rime.belghali@gmail.com>
2023-05-22 19:35:51 +0000

    pratice again

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 /**
-* print_alphabet: Print the alphabet
+* print_alphabet: Print the alphabet from a to z
 * Return: Always 0 on success
 */
 void print_alphabet(void)
